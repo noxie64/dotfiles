194bc7c94dd0712681231e97290db59a1d5369dc
noxie64 <the-real-nox@outlook.com>
2026-05-07 21:55:35 +0200

    Added layout cycling.

--- config/hypr/conf/awsm/hyprland.lua
+++ config/hypr/conf/awsm/hyprland.lua
@@ -44,11 +44,6 @@ hl.config({
     dwindle = {
         preserve_split = true,
     },
-
-    master = {
-        new_status = "master",
-    },
-
     misc = {
         force_default_wallpaper  = 0,
         disable_hyprland_logo    = true,
