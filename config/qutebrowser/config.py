080858069dd8355e1a9982ef18d97fa3209d3707
noxie64 <gkollegger5@outlook.com>
2026-05-17 21:36:19 +0200

    Added keybinds and misc settings.

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,14 +1,35 @@
+import os
+from urllib.request import urlopen
+
 config.load_autoconfig()
 
 c.fonts.default_family = "SpaceMono Nerd Font"
 c.fonts.default_size = "12pt"
+c.colors.webpage.preferred_color_scheme = "dark"
 
-import os
-from urllib.request import urlopen
+# tabs
+c.tabs.show = "switching"
+c.tabs.title.alignment = "center"
+c.tabs.show_switching_delay = 1500
+c.tabs.position = "bottom"
 
-# load your autoconfig, use this, if the rest of your config is empty!
-config.load_autoconfig()
+# statusbar
+c.statusbar.position = "top"
+c.statusbar.widgets = [
+        "url",
+        "tabs",
+        "progress"
+        ]
+c.statusbar.show = "in-mode"
 
+# keybinds
+config.bind(',b', 'spawn brave --new-window {url}') # if things dont work out, just open brave :)
+
+## rebind, makes more sense for me this way
+config.bind('J', 'tab-prev')
+config.bind('K', 'tab-next')
+
+# load color-scheme
 if not os.path.exists(config.configdir / "theme.py"):
     theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
     with urlopen(theme) as themehtml:
