df361de01cd101f6a7cc82124283c4bb9f81aa37
noxie64 <gkollegger5@outlook.com>
2026-08-17 15:24:02 +0200

    leftover changes.

--- config/hypr/conf/awsm/hyprland/env.lua
+++ config/hypr/conf/awsm/hyprland/env.lua
@@ -1,3 +1,7 @@
 hl.env("XCURSOR_SIZE",    "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("GTK_THEME",       "Catppuccin-Flamingo-Dark-Frappe")
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("GBM_BACKEND", "nvidia-drm")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("NVD_BACKEND", "direct")
